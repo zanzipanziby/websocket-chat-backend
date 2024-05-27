8c40bb26ba249bf1be2df10b76313f454aa75a01
ZanziPanziBY <zanzipanziby@gmail.com>
2024-05-27 22:47:41 +0300

    init: init chat app

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import http from 'http';
+import {Server} from 'socket.io';
+import cors from 'cors';
+
+
+const messages = [
+    {
+        message: "Test message",
+        id: "1243123",
+        user: {id: "qwerqwerqwer", name: "User"}
+    },
+]
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3001",  // Замените на адрес вашего клиента
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('message', (message) => {
+        console.log('Received message:', message);
+    });
+    socket.on('client-message-sent', message => {
+        const messageItem = {
+            message: message,
+            id: new Date().getTime().toString(),
+            user: {id: "qwerqwerqwer", name: "Zanzi"}
+        }
+        messages.push(messageItem)
+        console.log(message)
+        io.emit('new-message-sent', messageItem)
+    })
+
+    socket.on('disconnect', (reason) => {
+        console.log('Client disconnected due to:', reason);
+    });
+
+    socket.emit('init-messages', messages);
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
